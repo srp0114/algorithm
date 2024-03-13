243f30994c2d65261e8c71a16ed1810e7bef566d
young <92440521+srp0114@users.noreply.github.com>
2024-03-13 12:53:17 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.91 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,29 @@
+function solution(keymap, targets) {
+    var answer = [];    
+    const map = new Map();
+    
+    keymap.forEach((value, index) => {
+        let character = value.split("");
+        
+        character.forEach((ch, idx) => {
+            let check = map.get(ch)
+            if(!map.has(ch) || map.get(ch) > idx + 1) {
+                map.set(ch, idx + 1);
+            }
+        })
+    })
+    
+     targets.forEach(target => {
+        let sum = 0;
+        for (let ch of target) {
+            if (!map.has(ch)) {
+                sum = -1; 
+                break;
+            }
+            sum += map.get(ch);
+        }
+        answer.push(sum);
+    });
+    
+    return answer;
+}
