9386a88f760d472a0168f5565e82365fa197a71e
young <92440521+srp0114@users.noreply.github.com>
2024-03-13 16:40:02 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.48 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,16 @@
+function solution(wallpaper) {
+    let [lux, luy, rdx, rdy] = [51, 51, 0, 0]
+    
+    for(let i=0; i < wallpaper.length; i++) {
+        for(let j=0; j < wallpaper[i].length; j++) {
+            if(wallpaper[i][j] === "#") {
+                lux = Math.min(lux, i);
+                luy = Math.min(luy, j);
+                rdx = Math.max(rdx, i);
+                rdy = Math.max(rdy, j)
+            }
+        }
+    }
+
+    return [lux, luy, rdx + 1, rdy + 1];
+}
