1a858a0590d82ddccf030e895bb758314203a5af
young <92440521+srp0114@users.noreply.github.com>
2024-03-29 13:29:07 +0900

    [Silver III] Title: 1로 만들기, Time: 252 ms, Memory: 17496 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,21 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+const num = parseInt(input);
+
+const dp = Array.from({ length: num + 1 }).fill(0);
+
+for (let i = 2; i <= num; i++) {
+  dp[i] = dp[i - 1] + 1;
+
+  if (i % 2 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+  }
+  if (i % 3 === 0) {
+    dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  }
+}
+
+console.log(dp[num]);
