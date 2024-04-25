474b032c59ac78ee41ff27f6eae0f5521685436d
young <92440521+srp0114@users.noreply.github.com>
2024-04-25 15:10:59 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.42 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,25 @@
+function solution(number, limit, power) {
+    let result = 0;
+    
+    for(let i = 1; i <= number; i++) {
+        let count = 0;
+        
+        for(let j = 1; j <= Math.sqrt(i); j++) {
+            if (i%j === 0) {
+                if (i/j === j) {
+                    count += 1;
+                } else {
+                    count += 2;
+                }
+            }
+        }
+        
+        if(count > limit) {
+            result += power;
+        } else {
+            result += count;
+        }
+    }
+    
+    return result;
+}
