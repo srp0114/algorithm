a7690c41f39509836fa37c0fde843156107006c0
young <92440521+srp0114@users.noreply.github.com>
2024-04-29 16:03:42 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,16 @@
+function solution(ingredient) {
+    const set = [1, 2, 3, 1];
+    let hamburger = 0;
+
+    for(let i = 0; i < ingredient.length; i++) {
+        let arr = ingredient.slice(i, i+4);
+
+        if(arr.length === 4 && arr.toString() === set.toString()) {
+            ingredient.splice(i, 4);
+            hamburger++;
+            i-=3;
+        }
+    }
+
+    return hamburger;
+}
