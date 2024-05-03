f9e0466d79ab51d0275d6144b4fb521061b3a749
young <92440521+srp0114@users.noreply.github.com>
2024-05-03 15:15:47 +0900

    [level 1] Title: 콜라 문제, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -0,0 +1,11 @@
+function solution(a, b, n) {
+    let getCoke = 0;
+    
+    while(n >= a) {
+        const coke = Math.floor(n/a) * b;
+        n = n - (Math.floor(coke/b) * a) + coke;
+        getCoke += coke;
+    }
+    
+    return getCoke;
+}
