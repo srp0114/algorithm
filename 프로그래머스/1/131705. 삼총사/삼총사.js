6be1b40094702122527af83ccc45b89f9de91aac
young <92440521+srp0114@users.noreply.github.com>
2024-05-03 15:40:11 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,11 +1,10 @@
 function solution(number) {
-    let sum = Infinity;
     let three = 0;
     
-    for(let i = 0; i<number.length; i++) {
+    for(let i = 0; i < number.length; i++) {
         for(let j = i+1; j < number.length; j++) {
-            for(k = j + 1; k <number.length; k++) {
-                sum = number[i] + number[j] + number[k];
+            for(let k = j + 1; k < number.length; k++) {
+                const sum = number[i] + number[j] + number[k];
                 if(sum === 0) {
                     three++;
                 }
