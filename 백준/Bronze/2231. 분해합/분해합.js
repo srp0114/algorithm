b5d7adab40eafe1943d43233adf479d38f8f926a
young <92440521+srp0114@users.noreply.github.com>
2024-05-09 17:03:02 +0900

    [Bronze II] Title: 분해합, Time: 128 ms, Memory: 9604 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.js
+++ 백준/Bronze/2231. 분해합/분해합.js
@@ -1,18 +1,20 @@
-const num = +require("fs")
+//분해합 - 2231
+const input = require("fs")
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
   .toString()
   .trim()
   .split("\n");
 
+const num = +input[0];
+const tmp = num - 9 * input[0].length;
 let min = Infinity;
 
-for (let i = 1; i < num; i++) {
+for (let i = tmp; i < num; i++) {
   const n = i.toString().split("").map(Number);
 
   let sum = n.reduce((acc, val) => {
     return acc + val;
-  }, 0);
-  sum += i;
+  }, i);
 
   if (sum === num && sum < min) {
     min = i;
