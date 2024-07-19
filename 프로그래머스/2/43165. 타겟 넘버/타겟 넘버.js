5bbc6bf98823bf4d48297bb092f5a7e7d38215ac
young <92440521+srp0114@users.noreply.github.com>
2024-07-19 15:41:09 +0900

    [level 2] Title: 타겟 넘버, Time: 19.81 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,17 @@
+function solution(numbers, target) {
+    let count = 0;
+    
+    const dfs = (depth, sum) => {
+        if(depth === numbers.length) {
+            if(sum === target) count += 1;       
+            return;
+        } 
+        
+        dfs(depth + 1, sum + numbers[depth]);
+        dfs(depth + 1, sum - numbers[depth]);
+    };
+    
+    dfs(0, 0);
+    
+    return count;
+}
